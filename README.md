06b7d526239b5c7ad3605624fb3597a8112f7727
zhanengeng <zhanengeng@gmail.com>
2020-06-22 17:07:18 +0900

    工程フロー,requiredmentsを追加

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 # MyApps
 
-## 1.ToDoList
-### 環境
-mac  
-python3.8  
-django2.2  
+  
+
+# 1.ToDoList
+
+## 環境
+mac
 db:sqlite3
 
-### virtualenv
+| Package | Version |
+|--|--|
+python|3.8
+Django | 2.2
+pip |  20.1
+pytz| 2020.1
+setuptools| 46.1.3
+sqlparse| 0.3.1
+wheel | 0.34.2
--- myapp/apps/todolist/forms.py
+++ myapp/apps/todolist/forms.py
@@ -3,20 +3,20 @@ from django import forms
 # 任務追加用フォーム
 class ToDoAddForm(forms.Form):
     title = forms.CharField(label="Title",max_length=255)
-    contents = forms.CharField(label="内容", widget=forms.Textarea) # forms包中没有TextFeild
+    contents = forms.CharField(label="内容", widget=forms.Textarea, required=False) # forms包中没有TextFeild
     deadline = forms.DateField(label="締切日")
 
 
 # 任務削除用フォーム（論理削除）
 class ToDoDeleteForm(forms.Form):
     title = forms.CharField(label="Title",max_length=255)
-    contents = forms.CharField(label="内容", widget=forms.Textarea) 
+    contents = forms.CharField(label="内容", widget=forms.Textarea, required=False) 
     create_time = forms.DateTimeField(label="作成日")
     deadline = forms.DateField(label="締切日")
     done = forms.BooleanField(label="完成", required=False)
 
 class ToDoEditForm(forms.Form):
     title = forms.CharField(label="Title",max_length=255)
-    contents = forms.CharField(label="内容", widget=forms.Textarea) 
+    contents = forms.CharField(label="内容", widget=forms.Textarea, required=False) 
     deadline = forms.DateField(label="締切日")
     done = forms.BooleanField(label="完成", required=False)
